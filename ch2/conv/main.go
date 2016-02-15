70da5300c93e439c88bdd4f8c6570a13aa92d317
Sebastien Dejean <sdejean2298@gmail.com>
2016-02-14 19:25:49 -0800

    gopl, ch2, exercise 2.2 - a program like cf that can do general UNIT conversions, typo on previous commits

---
+++ ch2/conv/main.go
@@ -0,0 +1,89 @@
+/*
+ * Conv, a program to convert between SI and Imperial units of length, mass
+ * and temperature.
+ */
+package main
+
+import "flag"
+import "fmt"
+import "os"
+import "strconv"
+
+import "../unitconv/lengthconv"
+import "../unitconv/tempconv"
+
+func main() {
+	var f = flag.String("type", "", "type of unit specified, one of: length, mass, temp(erature), all")
+	flag.Parse()
+
+	var args = flag.Args()
+	switch {
+	case *f == "all":
+		fmt.Printf("Units of length:\n")
+		printLenConv(args)
+		fmt.Printf("Units of mass:\n")
+		printMassConv(args)
+		fmt.Printf("Units of temperature:\n")
+		printTempConv(args)
+	case *f == "length":
+		fmt.Printf("Units of length:\n")
+	case *f == "mass":
+		fmt.Printf("Units of mass:\n")
+	case *f == "temp":
+		fmt.Printf("Units of temperature:\n")
+		printTempConv(args)
+	case *f == "temperature":
+		fmt.Printf("Units of temperature:\n")
+		printTempConv(args)
+	default:
+		flag.PrintDefaults()
+	}
+}
+
+// printLenConv
+func printLenConv(args []string) {
+	for _, arg := range args {
+		t, err := strconv.ParseFloat(arg, 64)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "cfk: %v\n", err)
+			os.Exit(1)
+		}
+		m := lengthconv.Metre(t)
+		y := lengthconv.Yard(t)
+		f := lengthconv.Foot(t)
+		// i := lengthconv.Inch(t)
+		fmt.Printf("%s = %s, %s = %s, %s = %s\n",
+			m, lengthconv.MToY(m), m, lengthconv.MToF(m), m, lengthconv.MToI(m))
+		fmt.Printf("%s = %s, %s = %s, %s = %s\n",
+			y, lengthconv.YToM(y), y, lengthconv.YToF(y), y, lengthconv.YToI(y))
+		fmt.Printf("%s = %s, %s = %s, %s = %s\n",
+			f, lengthconv.FToM(f), f, lengthconv.FToY(f), f, lengthconv.FToI(f))
+		// fmt.Printf("%s = %s, %s = %s, %s = %s\n",
+		//	i, lengthconv.IToM(i), i, lengthconv.IToY(i), i, lengthconv.IToF(i))
+	}
+}
+
+// printMassConv
+func printMassConv(args []string) {
+	fmt.Println("TODO: MASS CONVERSIONS")
+}
+
+// printTempConv
+func printTempConv(args []string) {
+	for _, arg := range args {
+		t, err := strconv.ParseFloat(arg, 64)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "cfk: %v\n", err)
+			os.Exit(1)
+		}
+		c := tempconv.Celsius(t)
+		f := tempconv.Fahrenheit(t)
+		k := tempconv.Kelvin(t)
+		fmt.Printf("%s = %s, %s = %s\n",
+			c, tempconv.CToF(c), c, tempconv.CToK(c))
+		fmt.Printf("%s = %s, %s = %s\n",
+			f, tempconv.FToC(f), f, tempconv.FToK(f))
+		fmt.Printf("%s = %s, %s = %s\n",
+			k, tempconv.KToC(k), k, tempconv.KToF(k))
+	}
+}
